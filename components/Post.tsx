1f1fd90d8f3eef3fecd149e2caf624773b7e1637
badawy20oct <Badawy20oct@gmail.com>
2025-11-21 20:19:25 +0200

    .

--- components/Post.tsx
+++ components/Post.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/nextAuth";
 import DeletePostBtn from "./DeletePostBtn";
 
+
 interface NewsProps {
   id: string;
   author: string;
